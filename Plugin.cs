ee0527d7340d98c8816862dc07d1145a87c74215
distributivgesetz <distributivgesetz93@gmail.com>
2021-10-15 21:00:52 +0200

    add some hud messages

--- Plugin.cs
+++ Plugin.cs
@@ -14,7 +14,40 @@ namespace MrSandmanManMeASand
 		private GameObject _ply;
 		
 		private bool _isEnabled;
-		
+
+		private void Awake()
+		{
+			SceneManager.sceneLoaded += (scene, mode) =>
+			{
+				if (!_isEnabled)
+					return;
+
+				ObjectActivator trigger;
+				
+				switch (scene.name)
+				{
+					case "Level 2-4":
+						trigger = GameObject.Find("FirstRoom/Room/Cube (1)").GetComponent<ObjectActivator>();
+						trigger.events.onActivate.AddListener(() =>
+							HudMessageReceiver.Instance.SendHudMessage("The floor is unbearably hot, but it " +
+							                                           "seems like the tram is safe for you to stand on."));
+						break;
+					case "Level 3-2":
+						trigger = GameObject.Find("FirstRoom/Room/Cube (1)").GetComponent<ObjectActivator>();
+						trigger.events.onActivate.AddListener(() => 
+							HudMessageReceiver.Instance.SendHudMessage("The room feels cool, but you can feel " +
+																							"the blistering heat ahead."));
+						break;
+					case "Level 4-4":
+						trigger = GameObject.Find("FirstRoom/Room/Cube (1)").GetComponent<ObjectActivator>();
+						trigger.events.onActivate.AddListener(() => 
+							HudMessageReceiver.Instance.SendHudMessage("The air is hot and humid, but the floor " +
+																						"seems to be fine to walk on."));
+						break;
+				}
+			};
+		}
+
 		private void Update()
 		{
 			if (!_isEnabled)
@@ -39,7 +72,7 @@ namespace MrSandmanManMeASand
 				return;
 			}
 			
-			_isEnabled = GUI.Toggle(new Rect(10, Screen.height - 30, 200, 20), _isEnabled, "MrSandman enabled");
+			_isEnabled = GUI.Toggle(new Rect(10, Screen.height - 30, 200, 20), _isEnabled, "Enable MrSandman");
 		}
 	}
 
@@ -50,17 +83,18 @@ namespace MrSandmanManMeASand
 		private NewMovement _nm;
 		private GameObject _prefab;
 
-		private static readonly string[] DamageBlacklist =
+		private static readonly (string, string[])[] DamageBlacklist =
 		{
 			// Common
-			"FirstRoom", "FinalRoom", "Secret FirstRoom", "FinalRoom 2", "Prime FirstRoom", "Prime FinalRoom",
+			(null, new[] { "FirstRoom", "FinalRoom", "Secret FirstRoom", "FinalRoom 2", "Prime FirstRoom", "Prime FinalRoom" }),
+			("Level 0-1", new[] { "1 -  Starting Room", "2A - Hallway", "2B - Hallway", "1Alt -  Short Starting Room", "2Alt - Short Hallway" }),
 			// 2-4
-			"0 - Tram", 
+			("Level 2-4", new[] {"0 - Tram"}), 
 			// 3-2
-			"1 - Opening", "2 - Thin Walkway", "3 - Other Room", "3B - Drop",
+			("Level 3-2", new[] {"1 - Opening", "2 - Thin Walkway", "3 - Other Room", "3B - Drop", "DoorMouth" }),
 			// 4-4
-			"1 - Underground", "2 - Elevator 1", "3 - Ground Floor", "3B - Secret Shortcut", "3A - Elevator 2", "4 - Pit Bridge",
-			"5 - Window Hallway", "6 - Boss Entrance"
+			("Level 4-4", new[] { "1 - Underground", "2 - Elevator 1", "3 - Ground Floor", "3B - Secret Shortcut", "3A - Elevator 2", 
+									"4 - Pit Bridge", "5 - Window Hallway", "6 - Boss Entrance", "DoorGreed", "Exterior" })
 		};
 		
 		private void Start()
@@ -79,15 +113,17 @@ namespace MrSandmanManMeASand
 		{
 			while (true)
 			{
-				var col = (f_gc_currentCol?.GetValue(_gc) as Collider)?.gameObject;
-				if (_gc.onGround && DamageBlacklist.All(p => !IsParent(col, p)))
+				if (SceneManager.GetActiveScene().name == "Tutorial")
 				{
-					if (SceneManager.GetActiveScene().name == "Level 3-2" && IsParent(col, "DoorMouth") ||
-					    SceneManager.GetActiveScene().name == "Level 4-4" && (IsParent(col, "DoorGreed") || IsParent(col, "Exterior")))
-					{
-						yield return null;
-						continue;
-					}
+					yield return null;
+					continue;
+				}
+				var col = (f_gc_currentCol?.GetValue(_gc) as Collider)?.gameObject;
+				if (_gc.onGround && !DamageBlacklist
+					.Where(p => p.Item1 == null || p.Item1 == SceneManager.GetActiveScene().name)
+					.SelectMany(p => p.Item2)
+					.Any(p => IsParent(col, p)))
+				{
 					_nm.GetHurt(10, false);
 					Instantiate(_prefab, transform.position, Quaternion.identity);
 					yield return new WaitForSeconds(1f);
